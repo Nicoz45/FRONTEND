76ce64a35aed7f5c00a63f50e06e731996d572aa
Nicolas Zarate <nicolaszarate31@gmail.com>
2025-11-08 14:44:11 -0300

    Changed Styles

--- src/screens/HomeScreen/HomeScreen.jsx
+++ src/screens/HomeScreen/HomeScreen.jsx
@@ -3,7 +3,33 @@ import { useLocation } from 'react-router'
 
 const HomeScreen = () => {
     return (
-        <div>HomeScreen</div>
+        <div className='home-screen-container'>
+            <header className='general-header-container'>
+                <div className='logo'>
+                    <h1>SGPT</h1>
+                    <span>Sistema de Gestión de Proyectos y Tareas</span>
+                </div>
+                <nav className='nav-bar'>
+                    <ul>
+                        <li>Proyectos</li>
+                        <li>Tareas</li>
+                        <li>Usuarios</li>
+                        <li>Foro</li>
+                    </ul>
+                </nav>
+            </header>
+            <main className='Principal-screen'>
+                <div className="welcome-title">
+                    <h2>Bienvenido a SGPT</h2>
+                    <p>Tu sistema integral para la gestión eficiente de proyectos y tareas.</p>
+                </div>
+            </main>
+            <footer>
+                <div className='footer-text'>
+                    <span>© 2025 SGPT, Todos los derechos reservados</span>
+                </div>
+            </footer>
+        </div>
     )
 }
 
